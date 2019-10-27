8f0371facadbebb748421d5c297e22d642605cd2
Brian Jenkins <brianjenkins94@gmail.com>
2019-10-26 23:27:20 -0400

    Just needed to set `highWaterMark`

--- README.md
+++ README.md
@@ -34,6 +34,55 @@
 
 ### Obligatory
 
+#### FizzBuzz
+
+```typescript
+for (let i = 1; i <= 100; i++) {
+	if (i % 15 === 0) {
+		console.log("FizzBuzz");
+	} else if (i % 3 === 0) {
+		console.log("Fizz");
+	} else if (i % 5 === 0) {
+		console.log("Buzz");
+	} else {
+		console.log(i);
+	}
+}
+```
+
+#### Fibonacci (Iterative)
+
+```typescript
+function fibonacci(n) {
+	const array = [0, 1];
+
+	for (let i = 2; i <= n; i++) {
+		console.log(i);
+
+		array.push(array[i - 2] + array[i - 1]);
+	}
+
+	return array[n];
+}
+
+console.log(fibonacci(10));
+```
+
+#### Fibonacci (Recursive)
+
+```typescript
+function fibonacci(n) {
+	console.log(n);
+
+	if (n < 2) {
+		return n;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(10));
+```
 
 ### Second Category
 
@@ -48,17 +97,3 @@
 `Rosettas-Node` is licensed under the [MIT License](https://github.com/brianjenkins94/Rosettas-Node/blob/master/LICENSE).
 
 All files located in the `node_modules` directory are externally maintained libraries used by this software which have their own licenses; it is recommend that you read them, as their terms may differ from the terms in the MIT License.
-#### FizzBuzz
-
-```typescript
-```
-
-#### Fibonacci (Iterative)
-
-```typescript
-```
-
-#### Fibonacci (Recursive)
-
-```typescript
-```
--- index.ts
+++ index.ts
@@ -9,27 +9,25 @@ const inputFile = path.resolve(argv._[0]);
 const outputFile = path.resolve(argv._[1]);
 
 const inputFileReadStream = createInterface({
-	"input": fs.createReadStream(inputFile)
+	"input": fs.createReadStream(inputFile, { "highWaterMark": 1 })
 });
 
 const writeStream = fs.createWriteStream(outputFile);
 
-inputFileReadStream.on("line", function readLine(line) {
+inputFileReadStream.on("line", function readLine(line, readStream = inputFileReadStream) {
 	if (line.startsWith("@import") === true) {
-		inputFileReadStream.pause();
+		readStream.pause();
 
-		console.log(line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\"")));
-
-		const readStream = createInterface({
-			"input": fs.createReadStream(path.resolve(path.dirname(inputFile), line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""))))
+		const childReadStream = createInterface({
+			"input": fs.createReadStream(path.resolve(path.dirname(inputFile), line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""))), { "highWaterMark": 1 })
 		});
 
-		readStream.on("line", function(line) {
-			readLine(line);
+		childReadStream.on("line", function(line) {
+			readLine(line, childReadStream);
 		});
 
-		readStream.on("end", function() {
-			inputFileReadStream.resume();
+		childReadStream.on("close", function() {
+			readStream.resume();
 		});
 	} else {
 		writeStream.write(line + "\n");
