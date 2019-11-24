2ba7edb173660623a1ae69a1d53fa6453fb6abc4
Brian Jenkins <brianjenkins94@gmail.com>
2019-11-24 17:08:27 -0500

    Formatting tweak

--- index.ts
+++ index.ts
@@ -94,13 +94,13 @@ function expandableOutputBlock(cwd, line) {
 	return new Promise(function(resolve, reject) {
 		readStream.on("close", function() {
 			if (belowTheFold.length === 0) {
-				writeStream.write("<table><tbody><tr></tr><tr><td><strong>Sample Output:" + "&nbsp;".repeat(200) + "</strong>\n\n```\n" + aboveTheFold.join("\n") + "\n```\n</td></tr></tbody></table>\n");
+				writeStream.write("<table><tbody><tr></tr><tr><td><strong>Sample Output:" + "&nbsp;".repeat(209) + "</strong>\n\n```\n" + aboveTheFold.join("\n") + "\n```\n</td></tr></tbody></table>\n");
 			} else {
 				if (aboveTheFold[aboveTheFold.length - 1] === "") {
 					aboveTheFold.pop();
 				}
 
-				writeStream.write("<table><tbody><tr></tr><tr><td><details><summary><strong>Sample Output:" + "&nbsp;".repeat(200) + "</strong>\n\n```\n" + aboveTheFold.join("\n") + "\n```\n</summary>\n\n```\n" + belowTheFold.join("\n") + "\n```\n</details></td></tr></tbody></table>\n");
+				writeStream.write("<table><tbody><tr></tr><tr><td><details><summary><strong>Sample Output:" + "&nbsp;".repeat(204) + "</strong>\n\n```\n" + aboveTheFold.join("\n") + "\n```\n</summary>\n\n```\n" + belowTheFold.join("\n") + "\n```\n</details></td></tr></tbody></table>\n");
 			}
 
 			resolve();
