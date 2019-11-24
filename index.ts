24f5c0ff8e590475473cc1aa5c9fe9fe637db85c
Brian Jenkins <brianjenkins94@gmail.com>
2019-11-24 12:42:44 -0500

    Hack adjustments

--- index.ts
+++ index.ts
@@ -50,9 +50,9 @@ function expandableCodeBlock(cwd, line) {
 	return new Promise(function(resolve, reject) {
 		readStream.on("close", function() {
 			if (belowTheFold.length === 0) {
-				writeStream.write("<table><tbody><tr></tr><tr><td><strong>Solution:" + "&nbsp;".repeat(225) + "</strong>\n\n```" + language + "\n" + aboveTheFold.join("\n") + "\n```\n</td></tr></tbody></table>\n");
+				writeStream.write("<table><tbody><tr></tr><tr><td><strong>Solution:" + "&nbsp;".repeat(223) + "</strong>\n\n```" + language + "\n" + aboveTheFold.join("\n") + "\n```\n</td></tr></tbody></table>\n");
 			} else {
-				writeStream.write("<table><tbody><tr></tr><tr><td><details><summary><strong>Solution:" + "&nbsp;".repeat(225) + "</strong>\n\n```" + language + "\n" + aboveTheFold.join("\n") + "\n```\n</summary>\n\n```" + language + "\n" + belowTheFold.join("\n") + "\n```\n</details></td></tr></tbody></table>\n");
+				writeStream.write("<table><tbody><tr></tr><tr><td><details><summary><strong>Solution:" + "&nbsp;".repeat(218) + "</strong>\n\n```" + language + "\n" + aboveTheFold.join("\n") + "\n```\n</summary>\n\n```" + language + "\n" + belowTheFold.join("\n") + "\n```\n</details></td></tr></tbody></table>\n");
 			}
 
 			resolve();
